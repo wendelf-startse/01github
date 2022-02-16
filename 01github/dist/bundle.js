142ffe57d4245b670f68ff93194783f8ea092aa8
wendelf-startse <wendel.ferreira@startse.com>
2022-02-15 21:57:23 -0300

    first

---
+++ 01github/dist/bundle.js
@@ -0,0 +1,10 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+
+//import { App } from './App';
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, " Hello World ");
+}
